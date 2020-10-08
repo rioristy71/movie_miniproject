7a1c4d4a390bbaffdcbd92ebcf60dbf58e90eae7
rio <rioristy71@gmail.com>
2020-10-08 16:39:36 +0700

    searchlebar2

--- src/components/general/NavbarAtas.jsx
+++ src/components/general/NavbarAtas.jsx
@@ -5,7 +5,7 @@ export default function NavbarAtas() {
     return (
         <div>
            <Navbar bg="transparant" variant="dark" expand="lg">
-                <Navbar.Brand href="#home">M 0 V ! E</Navbar.Brand>
+                <Navbar.Brand href="#home">M 0 V ! E S!TE</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className=" mr-auto ">
