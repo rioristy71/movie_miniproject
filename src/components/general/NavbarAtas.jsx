30666a0cfafbb851d196f5b068c32b8f411ace90
blackwolfinc <inc.blackwolf@gmail.com>
2020-10-11 13:31:46 +0700

    tes

--- src/components/general/NavbarAtas.jsx
+++ src/components/general/NavbarAtas.jsx
@@ -8,11 +8,8 @@ export default function NavbarAtas() {
            <Navbar bg="transparant" variant="dark" expand="md">
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-<<<<<<< HEAD
-                <Navbar.Brand className="" to={'/'}>Icon</Navbar.Brand>
-=======
+
                 <Navbar.Brand className="logo" to={'/'}>C ! N E M A T ! C</Navbar.Brand>
->>>>>>> b5fe3c705353f398ad9e2c0242f7d320b6a4377a
                     <Nav className=" mr-auto ">
                     </Nav>
                    
